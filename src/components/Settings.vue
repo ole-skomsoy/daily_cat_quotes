93430bd5951c03745caadcb5b906f7c9b69bdf11
0138 <oleskomsoy@proton.me>
2025-06-30 10:31:27 +0200

    added dropdowns for time

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -25,32 +25,12 @@
 
 <template>
     <div>
-      <select class="dropdown" v-bind:value="settings.cat_time_hours">
-        <option value="0">0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-        <option value="11">11</option>
-        <option value="12">12</option>
-        <option value="12">13</option>
-        <option value="14">14</option>
-        <option value="15">15</option>
-        <option value="16">16</option>
-        <option value="17">17</option>
-        <option value="18">18</option>
-        <option value="19">19</option>
-        <option value="20">20</option>
-        <option value="21">21</option>
-        <option value="22">22</option>
-        <option value="23">23</option>
-        <option value="24">24</option>
+      <select class="hour_dropdown" v-bind:value="settings.cat_time_hours">
+        <option v-for="hour in 24" :key="index" :value="hour">{{hour}}</option>
+      </select>
+      <select class="minutes_dropdown" v-bind:value="settings.cat_time_minutes">
+        <option  value="0">0</option>
+        <option v-for="minutes in 60" :key="index" :value="minutes">{{minutes}}</option>
       </select>
     </div>
 </template>
