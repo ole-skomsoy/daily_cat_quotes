5145aa16449c8a832be1232e80cc058d359afcf4
0138 <oleskomsoy@proton.me>
2025-07-17 00:49:02 +0200

    dates somewhat under control

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,6 +1,8 @@
 <script setup>
     import { ref, onMounted } from 'vue';
     const next_cat_time = ref(null);
+    const hours_left = ref(0);
+    const minutes_left = ref(0);
 
     onMounted(() => {;
         setup_settings(); 
@@ -21,14 +23,24 @@
             next_alarm.setHours(12, 0, 0, 0);
             console.log('> Alarm has expired, new:', next_alarm);
         }
-        
+
         next_cat_time.value = next_alarm;
+        hours_left.value = ((next_cat_time.value - new Date()) / 1000 / 60 / 60).toFixed(0);
+        
+        let temp_date = new Date();
+        temp_date.setDate(temp_date.getDate() + 1);
+
+        minutes_left.value = Math.abs(next_cat_time.value.getMinutes() - temp_date.getMinutes());
     }
 </script>
 
 <template>
     <div>
-      <span>Next cat time: {{ next_cat_time }}</span>
+        <span>Next cat time</span>
+        <br>
+        <span>{{ next_cat_time }}</span> 
+        <br>
+        <span>({{ hours_left }} hours, {{ minutes_left }} minutes)</span>
     </div>
 </template>
 
