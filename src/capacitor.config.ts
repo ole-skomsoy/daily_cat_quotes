63e979a3948e491602a82a7bb8485fb2415cfcb1
0138 <oleskomsoy@proton.me>
2025-07-20 15:14:22 +0200

    updated capacitor config

---
+++ src/capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: '86f02a7a',
+  appName: 'Daily Cat Quotes',
+  webDir: 'www',
+};
+
+export default config;
